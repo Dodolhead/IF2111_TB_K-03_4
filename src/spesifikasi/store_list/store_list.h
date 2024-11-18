564fe7f0200cfdb58e249b0073fd8b698f2311d2
rasyidrizky <rasyid.rsn@gmail.com>
2024-11-18 23:41:54 +0700

    Update store_list.h

---
+++ src/spesifikasi/store_list/store_list.h
@@ -0,0 +1,25 @@
+#include "mesinkarakter.h"
+#include "mesinkata.h"
+
+void StoreList();
+/*
+Melihat barang-barang apa saja yang ada di dalam toko. 
+Setiap barang yang ditampilkan haruslah bersifat unique.
+
+Contoh input:
+STORE LIST
+
+Contoh output:
+{ Jika terdapat barang dalam toko }
+List barang yang ada di toko : 
+- Platypus Laser
+- Shrink Ray
+- Net Shooter
+- Camouflage Cloak
+- Sleep Dart Gun
+- Bubble Blaster
+
+{ Jika tidak ada barang dalam toko }
+TOKO KOSONG
+
+*/
