2f4024cd51544db9c2b73b31fc1f4521e37ce55e
Nicholas Zefanya <87974728+nicholaszef@users.noreply.github.com>
2024-11-18 15:07:03 +0700

    feat : add title

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
+/*
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#include "adt/boolean.h"
+#include "adt/list.h"
+#include "adt/mesinkarakter.h"
+#include "adt/mesinkata.h"  
+#include "adt/queue.h"
+#include "adt/stack.h"*/
 #include <stdio.h>
 
 int main(){
-    
+
+    printf("  _____  _    _ _____  _____  __  __          _____ _______ \n");
+    printf(" |  __ \\| |  | |  __ \\|  __ \\|  \\/  |   /\\   |  __ \\__   __|\n");
+    printf(" | |__) | |  | | |__) | |__) | \\  / |  /  \\  | |__) | | |   \n");
+    printf(" |  ___/| |  | |  _  /|  _  /| |\\/| | / /\\ \\ |  _  /  | |   \n");
+    printf(" | |    | |__| | | \\ \\| | \\ \\| |  | |/ ____ \\| | \\ \\  | |   \n");
+    printf(" |_|     \\____/|_|  \\_\\_|  \\_\\_|  |_/_/    \\_\\_|  \\_\\ |_|   \n");
+    printf("                                                            \n");
+    printf("(START) Start shopping | (CART) View your cart | (HELP) Help | (EXIT) Exit\n");
 }
