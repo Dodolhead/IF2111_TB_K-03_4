f46c44af3067546096408b711de1d02c5c1ccfb5
Rasyid Rizky Susilo Nurdwiputro <rasyid.rsn@gmail.com>
2024-11-19 22:47:02 +0700

    Update store_list.c

--- src/spesifikasi/store_list/store_list.c
+++ src/spesifikasi/store_list/store_list.c
@@ -1,42 +1,21 @@
-//#include <stdio.h>
+#include <stdio.h>
 #include "store_list.h"
 
-void StoreList() {
-    int i, j, barang = 0, baris = 1;
-    STARTWORD();
+void StoreList(ArrayDin Info) {
+    int i, j;
 
-    // Mengecek jumlah barang dalam toko
-    for (i = 0; i < CurrentWord.Length; i++) {
-        barang = barang * 10 + ((CurrentWord.TabWord[i]) - '0');
-    }
-
-    // Mencetak barang dalam toko
-    if (barang == 0) {
+    if (Length(Info) == 0) {
         printf("TOKO KOSONG");
     } else {
         printf("List barang yang ada di toko :\n");
-        
-        while (baris <= barang) {
-            ADVWORD();
-            ADVWORD();
-
+        for (i = 0; i < Length(Info); i++) {
             printf("- ");
-            for (j = 0; j < CurrentWord.Length; j++) {
-                printf("%c", CurrentWord.TabWord[j]);
+
+            for (j = 0; Info.A[i].name[j] != '\0'; j++) {
+                printf("%c", Info.A[i].name[j]);
             }
 
-            // Mencetak barang yang memiliki lebih dari satu kata
-            while (!EndWord && GetCC() != '\n') {
-                ADVWORD();
-
-                printf(" ");
-
-                for (j = 0; j < CurrentWord.Length; j++) {
-                    printf("%c", CurrentWord.TabWord[j]);
-                }
-            }
             printf("\n");
-            baris++;
         }
     }
 }
