f5b6d99920907574b5119d143cef195d28174afd
Rasyid Rizky Susilo Nurdwiputro <rasyid.rsn@gmail.com>
2024-11-21 19:43:40 +0700

    v1.0

--- src/adt/queue.c
+++ src/adt/queue.c
@@ -1,79 +1,85 @@
-#include "queue.h"
 #include <stdio.h>
+#include "queue.h"
 
-void CreateQueue(Queue *q){
-    q->idxHead = IDX_UNDEF;
-    q->idxTail = IDX_UNDEF;
-}
+/* *** Kreator *** */
+void CreateQueue(Queue *q) {
 /* I.S. sembarang */
 /* F.S. Sebuah q kosong terbentuk dengan kondisi sbb: */
 /* - Index head bernilai IDX_UNDEF */
 /* - Index tail bernilai IDX_UNDEF */
 /* Proses : Melakukan alokasi, membuat sebuah q kosong */
+    // KAMUS
+    // ALGORITMA
+    (*q).idxHead = IDX_UNDEF;
+    (*q).idxTail = IDX_UNDEF;
+}
 
 /* ********* Prototype ********* */
-boolean isEmpty(Queue q){
-    return q.idxHead == IDX_UNDEF && q.idxTail == IDX_UNDEF;
-}
+boolean isEmpty(Queue q) {
 /* Mengirim true jika q kosong: lihat definisi di atas */
-boolean isFull(Queue q){
-    return (q.idxTail + 1) % CAPACITY == q.idxHead;
+    // KAMUS
+    // ALGORITMA
+    return (q.idxHead == IDX_UNDEF) && (q.idxTail == IDX_UNDEF);
 }
+
+boolean isFull(Queue q) {
 /* Mengirim true jika tabel penampung elemen q sudah penuh */
 /* yaitu IDX_TAIL akan selalu di belakang IDX_HEAD dalam buffer melingkar*/
-
-int length(Queue q){
-    int count =0;
-    if (isEmpty(q)){
-        return 0;
-    }
-    if (isFull(q)){
-        return CAPACITY;
-    }
-    else return q.idxTail - q.idxHead + 1;
+    // KAMUS
+    // ALGORITMA
+    return ((q.idxHead == 0) && (q.idxTail == CAPACITY-1));
 }
+
+int length(Queue q) {
 /* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika q kosong. */
+    // KAMUS
+    // ALGORITMA
+    if (isEmpty(q)) {
+        return 0;
+    } else {
+        return q.idxTail - q.idxHead + 1;
+    }
+}
 
 /* *** Primitif Add/Delete *** */
-void enqueue(Queue *q, ElType val){
-    if (isEmpty(*q)){
-        q->idxHead = 0;
-        q->idxTail = 0;
-    }
-    else{
-        q->idxTail = (q->idxTail + 1) % CAPACITY;
-    }
-    TAIL(*q) = val;
-}
+void enqueue(Queue *q, char* name) {
 /* Proses: Menambahkan val pada q dengan aturan FIFO */
 /* I.S. q mungkin kosong, tabel penampung elemen q TIDAK penuh */
 /* F.S. val menjadi TAIL yang baru, IDX_TAIL "mundur" dalam buffer melingkar. */
+    // KAMUS
+    int i = 0;
+    // ALGORITMA
+    if (isEmpty(*q)) {
+        (*q).idxHead = 0;
+        (*q).idxTail = 0;
+    } else {
+        (*q).idxTail = ((*q).idxTail + 1) % CAPACITY;
+    }
 
-void dequeue(Queue *q, ElType *val){
-    *val = HEAD(*q);
-    if (IDX_HEAD(*q) == IDX_TAIL(*q)){
-        q->idxHead = IDX_UNDEF;
-        q->idxTail = IDX_UNDEF;
-    }
-    else{
-        q->idxHead = (q->idxHead + 1) % CAPACITY;
-    }
+    copyString(TAIL(*q).name, name);
 }
+
+
+void dequeue(Queue *q, char *name) {
 /* Proses: Menghapus val pada q dengan aturan FIFO */
 /* I.S. q tidak mungkin kosong */
-/* F.S. val = nilai elemen HEAD pd I.S., IDX_HEAD "mundur";
+/* F.S. name = nilai elemen HEAD pd I.S., IDX_HEAD "mundur";
         q mungkin kosong */
+    // KAMUS
+    int i = 0;
+    // ALGORITMA
+    copyString(name, HEAD(*q).name);
+    
+    if ((*q).idxHead == (*q).idxTail) {
+        (*q).idxHead = IDX_UNDEF;
+        (*q).idxTail = IDX_UNDEF;
+    } else {
+        (*q).idxHead = (*q).idxHead + 1;
+    }
+}
 
 /* *** Display Queue *** */
-void displayQueue(Queue q){
-    printf("[");
-    int i = q.idxHead;
-    while (i != q.idxTail){
-        printf("%d,",q.buffer[i]);
-        i = (i + 1) % CAPACITY;
-    }
-    printf("%d]\n",TAIL(q));
-}
+void displayQueue(Queue q) {
 /* Proses : Menuliskan isi Queue dengan traversal, Queue ditulis di antara kurung 
    siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan 
    karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
@@ -81,15 +87,33 @@ void displayQueue(Queue q){
 /* F.S. Jika q tidak kosong: [e1,e2,...,en] */
 /* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
 /* Jika Queue kosong : menulis [] */
-
-// int main(){
-//     Queue q;
-//     int val;
-//     CreateQueue(&q);
-//     enqueue(&q,1);
-//     enqueue(&q,2);
-//     enqueue(&q,3);
-//     displayQueue(q);
-//     printf("%d\n",length(q));
-// }
-
+    // KAMUS
+    int i, j;
+    // ALGORITMA
+    if (isEmpty(q)) {
+        printf("[]\n");
+    } else {
+        printf("[\n");
+        for (i = q.idxHead; i <= q.idxTail; i++) {
+            j = 0;
+            if (i == q.idxTail) {
+                printf("Harga: %d ", q.buffer[i].price);
+                printf("Barang: ");
+                while (q.buffer[i].name[j] != '\0') {
+                    printf("%c", q.buffer[i].name[j]);
+                    j++;
+                }
+                printf("\n");
+            } else {
+                printf("Harga: %d ", q.buffer[i].price);
+                printf("Barang: ");
+                while (q.buffer[i].name[j] != '\0') {
+                    printf("%c", q.buffer[i].name[j]);
+                    j++;
+                }
+                printf(",\n");
+            }
+        }
+        printf("]\n");
+    }
+}
