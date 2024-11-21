e122dda17fd0335e29b3303d9b9e86db8dfed649
Rasyid Rizky Susilo Nurdwiputro <rasyid.rsn@gmail.com>
2024-11-21 18:44:48 +0700

    v1.0

--- src/adt/barang.h
+++ src/adt/barang.h
@@ -1,6 +1,8 @@
 #ifndef BARANG_H
 #define BARANG_H
 
+#include "../../dictionary.h"
+
 #define MAX_LEN 100
 #define NULL -1
 
@@ -8,5 +10,21 @@ typedef struct {
         char name[MAX_LEN];
         int price;
 } Barang;
+
+/* ********** KONSTRUKTOR BARANG ********** */
+Barang MakeBarang(char* name, int price);
+/* Membentuk sebuah Barang dari komponen-komponennya */
+/* Prekondisi (tidak perlu dicek): */
+/* Name terdiri dari maksimal 100 karakter  */
+/* price terdiri dari bilangan bulat */
+/* I.S. name, price terdefinisi */
+/* F.S. Barang terbentuk dengan name dan price yang sesuai */
+
+/* ********** SELEKTOR ********** */
+int HargaBarang(Barang b);
+/* Mengirimkan harga barang */
+
+char* NamaBarang(Barang b);
+/* Mengirimkan nama barang */
 
 #endif
