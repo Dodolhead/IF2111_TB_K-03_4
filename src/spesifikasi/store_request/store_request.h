85818de2e830c34e5f6ec2b2a74cdc122df16d8b
Rasyid Rizky Susilo Nurdwiputro <rasyid.rsn@gmail.com>
2024-11-22 13:10:45 +0700

    v1.0

--- src/spesifikasi/store_request/store_request.h
+++ src/spesifikasi/store_request/store_request.h
@@ -1,29 +1,29 @@
-#include "queue.h"
-#include "arraydinBarang.h"
-#include "boolean.h"
+#include "..\..\adt\queue.h"
+#include "..\..\adt\arraydinBarang.h"
+#include "..\..\adt\boolean.h"
 
-void StoreRequest();
+void StoreRequest(Queue q, ArrayDin Info);
 /*
 Meminta penambahan barang baru ke dalam toko.
 Barang-barang yang diminta akan disimpan di dalam sebuah antrian dan akan dimasukkan ke toko menggunakan command selanjutnya. 
 Nama barang yang masuk tidak boleh sama dengan nama barang yang sudah ada di toko atau di antrian.
 
 Contoh input:
-STORE REQUEST
+>> STORE REQUEST
 Nama barang yang diminta: AK47
 
 Contoh output:
 Barang berhasil ditambahkan ke toko
 
 Contoh input:
-STORE REQUEST
+>> STORE REQUEST
 Nama barang yang diminta: Adaditoko
 
 Contoh output:
 Barang dengan nama yang sama sudah ada di toko!
 
 Contoh input:
-STORE REQUEST
+>> STORE REQUEST
 Nama barang yang diminta: Adadiantrian
 
 Contoh output:
