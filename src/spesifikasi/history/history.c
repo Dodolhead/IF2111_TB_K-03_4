9047f374926c8fdb74c47e432b124270177d7e98
Rasyid Rizky Susilo Nurdwiputro <rasyid.rsn@gmail.com>
2024-12-17 03:10:50 +0700

    feat

---
+++ src/spesifikasi/history/history.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "history.h"
+
+void History(Stack history, int N) {
+    /* Input berupa Stack history dengan tipe elemen barang (nama dan harga) dan
+        N jumlah barang yang ingin muncul */
+    // KAMUS
+    // ALGORITMA
+    if (!IsEmpty(history)) {
+        Stack temp;
+        char[100] name;
+        int cost, i;
+        CreateEmpty(&temp);
+
+        printf("Riwayat pembelian barang:\n");
+
+        for (i = 1; i <= N && !IsEmpty(history); i++) {
+            printf("%d. ", i);
+
+            while ((history).T[(history).TOP].name[i] != '\0') {
+                printf("%c", (history).T[(history).TOP].name[i]);
+            }
+
+            printf(" %d\n", HargaBarang(history.price));
+            Pop(&history, &name, &cost);
+            Push(&temp, name, cost);
+        }
+
+        for (i = 1; i <= N && !IsEmpty(history); i++) {
+            Pop(&temp, &name, &cost);
+            Push(&history, name, cost);
+        }
+    } else {
+        printf("Kamu belum membeli barang apapun!\n");
+    }
+}
